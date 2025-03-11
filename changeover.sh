6fa77f1c70afa7a54218dd0abdc7bceb0e5c47f7
Philip James <phildini@phildini.net>
2025-03-10 20:27:48 -0700

    Get ready for deploy

---
+++ changeover.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ../civic-band/ && \
+    /usr/bin/docker compose down sites_datasette_blue && \
+    cd ../corkboard/ && \
+    /usr/bin/docker compose up sites_datasette_blue -d --remove-orphans && \
+    cd ../civic-band/ && \
+    /usr/bin/docker compose down sites_datasette_green && \
+    cd ../corkboard/ && \
+    /usr/bin/docker compose up sites_datasette_green -d --remove-orphans && \

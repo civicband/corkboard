1bad1ae11ee2aa52e99d59ad480c6ef481cabb50
Philip James <phildini@phildini.net>
2025-05-23 21:11:26 -0700

    Sentry track errors on the asgi app

--- config/asgi.py
+++ config/asgi.py
@@ -9,10 +9,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 import djp
-
+import sentry_sdk
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = djp.asgi_wrapper(get_asgi_application())
+sentry_sdk.init(
+    "https://11ecb14e4a3249129b039b57090320c1@andromeda.nebulosa-moth.ts.net/3",
+
+    send_default_pii=True,
+    max_request_body_size="always",
+    sample_rate=0.25,
+    traces_sample_rate=0,
+)
+
+application = SentryAsgiMiddleware(djp.asgi_wrapper(get_asgi_application()))

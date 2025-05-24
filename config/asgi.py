33da66d7b77f971695394aeb9e5ee6f0280fab0c
Philip James <phildini@phildini.net>
2025-05-23 21:56:04 -0700

    Ok, but really stop sentry for now

--- config/asgi.py
+++ config/asgi.py
@@ -16,13 +16,15 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-sentry_sdk.init(
-    "https://11ecb14e4a3249129b039b57090320c1@andromeda.nebulosa-moth.ts.net/3",
+# sentry_sdk.init(
+#     "https://11ecb14e4a3249129b039b57090320c1@andromeda.nebulosa-moth.ts.net/3",
 
-    send_default_pii=True,
-    max_request_body_size="always",
-    sample_rate=0.25,
-    traces_sample_rate=0,
-)
+#     send_default_pii=True,
+#     max_request_body_size="always",
+#     sample_rate=0.25,
+#     traces_sample_rate=0,
+# )
 
-application = SentryAsgiMiddleware(djp.asgi_wrapper(get_asgi_application()))
+application = djp.asgi_wrapper(get_asgi_application())
+
+# application = SentryAsgiMiddleware(application)

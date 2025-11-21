5ccdbf7b5c6b2080408411862d7b6ffe28c56c60
Philip James <phildini@phildini.net>
2025-11-20 21:06:54 -0800

    Link to the right search from page details

--- plugins/date_link.py
+++ plugins/date_link.py
@@ -9,8 +9,11 @@ def render_cell(row, value, column, table, database, datasette, request):
         return None
     parts = urlparse(request.url)
     path = parts.path
+    path_parts = path.split("/")
+    if len(path_parts) > 3:
+        path = "/".join(path_parts[:3])
     if "agendas" not in path or "minutes" not in path:
-        path = parts.path.replace("upcoming", "agendas")
+        path = path.replace("upcoming", "agendas")
     url = f"{path}/?date__exact={value}&_sort=page"
     if "_search" in request.args:
         url += f"&_highlight={request.args['_search']}"

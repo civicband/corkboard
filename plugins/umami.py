fa422a4ac6f39b8624a52322ea5b726829348bb6
Philip James <phildini@phildini.net>
2025-11-23 15:59:32 -0800

    Try out posthog tracking

--- plugins/umami.py
+++ plugins/umami.py
@@ -4,5 +4,5 @@ from datasette import hookimpl
 @hookimpl
 def extra_body_script(datasette):
     return {
-        "script": f'</script><script src="https://analytics.civic.band/sunshine" data-website-id="6250918b-6a0c-4c05-a6cb-ec8f86349e1a" data-auto-track="false">'
+        "script": '</script><script src="https://analytics.civic.band/sunshine" data-website-id="6250918b-6a0c-4c05-a6cb-ec8f86349e1a" data-auto-track="false">'
     }

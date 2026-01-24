ae235b8d4da99c788cf41524cfdd1033f5beaa7a
Philip James <phildini@phildini.net>
2026-01-24 12:52:00 -0800

    Add Site model for sites.db access

--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,23 @@
-# Create your models here.
+from django.db import models
+
+
+class Site(models.Model):
+    """Municipality site in the CivicBand directory."""
+    subdomain = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    kind = models.CharField(max_length=100)
+    pages = models.IntegerField(default=0)
+    last_updated = models.DateTimeField()
+    lat = models.CharField(max_length=50, blank=True)
+    lng = models.CharField(max_length=50, blank=True)
+    popup = models.JSONField(default=dict, blank=True)
+
+    class Meta:
+        db_table = 'sites'
+        managed = False  # Don't let Django manage this table
+        ordering = ['-pages']
+
+    def __str__(self):
+        return f"{self.name} ({self.subdomain})"
---
+++ tests/test_site_model.py
@@ -0,0 +1,24 @@
+import pytest
+from pages.models import Site
+
+
+@pytest.mark.django_db
+class TestSiteModel:
+    def test_site_model_exists(self):
+        """Site model should be importable."""
+        assert Site is not None
+
+    def test_site_model_fields(self):
+        """Site model should have expected fields."""
+        fields = [f.name for f in Site._meta.get_fields()]
+        expected_fields = [
+            'subdomain', 'name', 'state', 'country', 'kind',
+            'pages', 'last_updated', 'lat', 'lng', 'popup'
+        ]
+        for field in expected_fields:
+            assert field in fields
+
+    def test_site_model_str(self):
+        """Site model should have string representation."""
+        site = Site(subdomain='test.ca', name='Test City')
+        assert str(site) == 'Test City (test.ca)'
